eec449f7d1b295812435b847fa8f314a3dbefa57
Tanishgol <tanish.gol117726@marwadiuniversity.ac.in>
2025-03-19 10:24:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+        <h1 className="bg-cyan-300 text-yellow-500 font-semibold text-4xl text-center">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
